2f2a5c949c9097040482d1a77c4503c90a154f8d
Lubo <lubomir999@abv.bg>
2017-10-28 14:39:40 +0300

    Upload Web Server
    Upload Web Server

---
+++ Web Server/MyCoolWebServer/Server/Routing/Contracts/IServerRouteConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCoolWebServer.Server.Routing.Contracts
+{
+    using Enums;
+    using System.Collections.Generic;
+
+    public interface IServerRouteConfig
+    {
+        IDictionary<HttpRequestMethod, IDictionary<string, IRoutingContext>> Routes { get; }
+
+        ICollection<string> AnonymousPaths { get; }
+    }
+}
