9eb7a23ad188a59027dd447aded588619cd9a2dd
Lubo <lubomir999@abv.bg>
2017-10-28 14:38:32 +0300

    Upload Mod Panel
    Upload Mod Panel

---
+++ Mod Panel Exam Preparation/ModPanel.App/Launcher.cs
@@ -0,0 +1,29 @@
+﻿namespace ModPanel.App
+{
+    using Infrastructure;
+    using Infrastructure.Mapping;
+    using Microsoft.EntityFrameworkCore;
+    using ModPanel.App.Data;
+    using SimpleMvc.Framework;
+    using SimpleMvc.Framework.Routers;
+    using WebServer;
+
+    public class Launcher
+    {
+        static Launcher()
+        {
+            using (var db = new ModPanelDbContext())
+            {
+                db.Database.Migrate();
+            }
+
+            AutoMapperConfiguration.Initialize();
+        }
+
+        public static void Main()
+            => MvcEngine.Run(new WebServer(
+                1337, 
+                DependencyControllerRouter.Get(),
+                new ResourceRouter()));
+    }
+}
