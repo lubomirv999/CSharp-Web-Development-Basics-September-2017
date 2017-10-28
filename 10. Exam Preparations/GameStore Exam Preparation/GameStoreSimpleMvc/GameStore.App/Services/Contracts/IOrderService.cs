6324549e283cf6bbb09437fb5c1af2ef42e34fc5
Lubo <lubomir999@abv.bg>
2017-10-28 14:33:20 +0300

    Fix files
    Fix files

--- 10. Exam Preparations/GameStore Exam Preparation/GameStoreSimpleMvc/GameStore.App/Services/Contracts/IOrderService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameStore.App.Services.Contracts
-{
-    using System.Collections.Generic;
-
-    public interface IOrderService
-    {
-        void Purchase(int userId, IEnumerable<int> gameIds);
-    }
-}
