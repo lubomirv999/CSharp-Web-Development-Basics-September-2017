38f6bb018279b8761a540091e63fadc85d7796b5
Lubo <lubomir999@abv.bg>
2017-10-28 14:35:25 +0300

    Upload Preparations For The Exam
    Upload Preparations For The Exam

---
+++ 10. Preparations For The Exam/GameStore Exam Preparation/GameStoreSimpleMvc/GameStore.App/Launcher.cs
@@ -0,0 +1,29 @@
+﻿namespace GameStore.App
+{
+    using Data;
+    using Infrastructure;
+    using Infrastructure.Mapping;
+    using Microsoft.EntityFrameworkCore;
+    using SimpleMvc.Framework;
+    using SimpleMvc.Framework.Routers;
+    using WebServer;
+
+    public class Launcher
+    {
+        static Launcher()
+        {
+            using (var db = new GameStoreDbContext())
+            {
+                db.Database.Migrate();
+            }
+
+            AutoMapperConfiguration.Initialize();
+        }
+
+        public static void Main()
+            => MvcEngine.Run(
+                new WebServer(1337, 
+                    DependencyControllerRouter.Get(), 
+                    new ResourceRouter()));
+    }
+}
